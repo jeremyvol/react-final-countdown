af8fcde441a4c581eabcdd34bbc669380c784697
Jeremy Vollant <j.vollant@gmail.com>
2025-06-08 20:16:01 +0200

    Exposing Component APIs via the useImperativeHandle Hook

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,7 +1,9 @@
 import { useRef, useState } from 'react';
+import ResultModal from './ResultModal.jsx';
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
+  const dialog = useRef();
 
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
@@ -10,6 +12,7 @@ export default function TimerChallenge({ title, targetTime }) {
     setTimerStarted(true);
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.open();
     }, targetTime * 1000);
   }
 
@@ -19,20 +22,22 @@ export default function TimerChallenge({ title, targetTime }) {
   }
 
   return (
-    <section className='challenge'>
-      <h2>{title}</h2>
-      {timerExpired && <p>You lost!</p>}
-      <p className='challenge-time'>
-        {targetTime} second{targetTime > 1 ? 's' : ''}
-      </p>
-      <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>
-          {timerStarted ? 'Stop' : 'Start'} Challenge
-        </button>
-      </p>
-      <p className={timerStarted ? 'active' : ''}>
-        {timerStarted ? 'Time is running...' : 'Timer inactive'}
-      </p>
-    </section>
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime} result='lost' />
+      <section className='challenge'>
+        <h2>{title}</h2>
+        <p className='challenge-time'>
+          {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? 'active' : ''}>
+          {timerStarted ? 'Time is running...' : 'Timer inactive'}
+        </p>
+      </section>
+    </>
   );
 }
